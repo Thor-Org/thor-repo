197b6b6f26b747ea1b84bee237dcefa4e7372dbf
jtemel11 <jtemel2018@my.fit.edu>
2021-12-07 01:01:22 -0500

    Created Outline for Seed Generator

---
+++ src/python_scripts/seedGenerator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+# Returns data frame (df) and word list of all fields imported
+def read_data():
+    """
+    Should read data from table in data base.
+    Write another script that imports the data from and Ascii file,
+    perform 'clean up tasks', and store in table in database
+    """
