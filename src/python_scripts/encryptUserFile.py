7d0b3520c4ef46973f596ba16a5b2de755b4ab96
Adonay <adonaypichardo@gmail.com>
2022-02-13 20:55:42 -0500

    AES encryption is working

--- src/python_scripts/encryptUserFile.py
+++ src/python_scripts/encryptUserFile.py
@@ -10,8 +10,8 @@ File:_> myLittleFile.txt
 Encryption:_> AES
 Key:_> 237692730480928346
 
-myLittleFile_encrypted
-myLittleFile_receipt
+myLittleFile_ENCRYPTED
+myLittleFile_RECEIPT
 """
 # Standard Imports
 from sys import argv, stdout
@@ -21,8 +21,7 @@ import base64
 from connectThorDatabase import connectThorDatabase
 
 # None Standard Imports
-from Crypto.Cipher import AES # pip3 install pycrypto, then pip3 pycryptodome
-from Crypto.Random import get_random_bytes
+from Cryptodome.Cipher import AES # pip3 install pycrypto, then pip3 pycryptodome
 
 def getCombination():
     connection = connectThorDatabase()
@@ -75,13 +74,15 @@ def recordKeyUsed(key):
 def main():
     # Read in file
     targetFile = open("../../uploads/" + argv[1], 'r')
-    targetFile = targetFile.read().encode("utf8")
-    stdout.write(f'ORIGINAL\n{targetFile}\n')
+    targetFile = targetFile.read()
+
+    # Encode for AES
+    targetFile = targetFile.encode("utf8")
 
     # Get combination from Database
     key = getCombination()
-
-    stdout.write(f'key = {key}\n')
+    # AES requires a key divisible by 16
+    key += key[0:6]
 
     # Record key used
     recordKeyUsed(key)
@@ -93,19 +94,30 @@ def main():
     # Encrypt
     ################################
     # Key must be 16 bytes
-    key += key[0:6]
+    key = key.encode("utf8")
 
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(targetFile)
 
-    file_out = open("encrypted.bin", "wb")
-    [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
-    file_out.close()
+    fileOut = open("../../uploads/" + argv[1] + "_ENCRYPTED", "wb")
+    [ fileOut.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+    fileOut.close()
+
+    fileOut = open("../../uploads/" + argv[1] + "_RECEIPT", "w")
+    fileOut.write(f'File:_> {argv[1]}\nEncryption:_> AES\nKey:_> {str(key)[2:-3]}\n')
+    fileOut.close()
 
     # ################################
     # # Decrypt
     # ################################
+    fileIn = open("../../uploads/" + argv[1] + "_ENCRYPTED", "rb")
+    nonce, tag, ciphertext = [ fileIn.read(x) for x in (16, 16, -1) ]
 
+    # let's assume that the key is somehow available again
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    data = cipher.decrypt_and_verify(ciphertext, tag)
+
+    stdout.write(f'\n{str(data)[2:-3]}\n')
 
 if __name__ == "__main__":
     main()
