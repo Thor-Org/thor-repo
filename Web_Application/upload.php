0badb0fec2ef3ec686ef60db9bf27cf6bbedd04e
Luke (PC) <Lukejb14@users.noreply.github.com>
2022-02-14 00:22:13 -0500

    Ditched previous function. Trying new upload function

--- Web_Application/upload.php
+++ Web_Application/upload.php
@@ -47,8 +47,3 @@ if ($uploadOk == 0) {
   }
 }
 ?>
-<br>
-<br>
-<br>
-Complete PHP Filesystem Reference
-For a complete reference of filesystem functions, go to our complete PHP Filesystem Reference.
