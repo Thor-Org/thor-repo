3ac87a2738be58ec772c57350d75cc92d4b71397
Luke (PC) <Lukejb14@users.noreply.github.com>
2022-02-14 11:50:53 -0500

    Update playground.php

--- Web_Application/playground.php
+++ Web_Application/playground.php
@@ -2,7 +2,6 @@ Python command:
 <?php
 $command1 = escapeshellcmd("python3 encryptUserFile.py test.txt");
 $output = shell_exec($command1);
-echo implode("", $output);
 echo "<br>Hash: ";
 echo $output;
 ?>
